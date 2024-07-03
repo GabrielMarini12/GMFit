010b8bef01ab2ae2140639aec621a18a8840ab46
Gabriel Marini <gabrielmarini12@icloud.com>
2024-07-02 22:25:48 -0300

    criado o contexto para verificar se tem usuario logado.

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,4 +1,6 @@
 import { ReactNode, createContext, useState, useEffect } from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "../services/firebaseConnection";
 
 interface AuthProviderProps {
   children: ReactNode;
@@ -6,6 +8,7 @@ interface AuthProviderProps {
 
 type AuthContextData = {
   signed: boolean;
+  loadingAuth: boolean;
 };
 
 interface UserProps {
@@ -18,9 +21,32 @@ export const AuthContext = createContext({} as AuthContextData);
 
 function AuthProvider({ children }: AuthProviderProps) {
   const [user, setUser] = useState<UserProps | null>(null);
+  const [loadingAuth, setLoadingAuth] = useState(true);
+
+  useEffect(() => {
+    const unsub = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        // tem usuario logado
+        setUser({
+          uid: user.uid,
+          name: user?.displayName,
+          email: user?.email,
+        });
+        setLoadingAuth(false);
+      } else {
+        // não tem usuario logado
+        setUser(null);
+        setLoadingAuth(false);
+      }
+    });
+
+    return () => {
+      unsub();
+    };
+  }, []);
 
   return (
-    <AuthContext.Provider value={{ signed: !!user }}>
+    <AuthContext.Provider value={{ signed: !!user, loadingAuth }}>
       {children}
     </AuthContext.Provider>
   );
