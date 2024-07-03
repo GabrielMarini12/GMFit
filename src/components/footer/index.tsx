bb9a1b5d7c91c49e9f0762a4e45720e8c3aab5c2
Gabriel Marini <gabrielmarini12@icloud.com>
2024-07-03 19:42:52 -0300

    Ajuste na logo do site.

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,10 +1,10 @@
-import logoImg from "../../assets/logo-academia.png";
+import logoImg from "../../assets/logoGmFit.png";
 
 export function Footer() {
   return (
     <footer className="w-full flex flex-col justify-center items-center bg-gray-950 pb-8 mt-8">
-      <img src={logoImg} alt="Logo so site" className="w-48" />
-      <p className="text-yellow-500 uppercase text-sm mt-[-35px] mb-2">
+      <img src={logoImg} alt="Logo so site" className="w-28 mt-4" />
+      <p className="text-yellow-500 uppercase text-sm mt-2 mb-2">
         Academia Oficial{" "}
         <small className="text-white uppercase text-[8px]">
           Do comitê Olímpico do Brasil
--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,17 +1,17 @@
 import { useContext } from "react";
 import { AuthContext } from "../../contexts/AuthContext";
 import { Link } from "react-router-dom";
-import logoImg from "../../assets/logo-academia.png";
+import logoImg from "../../assets/logoGmFit.png";
 import { FiUser, FiLogIn } from "react-icons/fi";
 
 export function Header() {
   const { signed, loadingAuth } = useContext(AuthContext);
 
   return (
-    <div className="w-full flex items-center justify-center h-28 bg-gray-950 mb-16 sticky top-0 shadow-xl shadow-slate-300">
+    <div className="w-full flex items-center justify-center h-28 bg-gray-950 mb-10 sticky top-0 shadow-xl shadow-slate-300">
       <header className="flex w-full max-w-7xl items-center justify-between px-4 mx-auto">
         <Link to="/">
-          <img src={logoImg} alt="Logo do site" className="h-64" />
+          <img src={logoImg} alt="Logo do site" className="h-28" />
         </Link>
 
         {!loadingAuth && !signed && (
