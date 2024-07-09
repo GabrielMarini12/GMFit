1360eccaa49e1f2d8472f56f706bc8c69ace2e0e
Gabriel Marini <gabrielmarini12@icloud.com>
2024-07-08 21:36:23 -0300

    Finalizado o layout da página de detalhes da academia.

--- src/pages/academias/index.tsx
+++ src/pages/academias/index.tsx
@@ -3,6 +3,7 @@ import { useParams, Link } from "react-router-dom";
 import { api } from "../../services/api";
 import { Container } from "../../components/container";
 import { AcademiaProps } from "../home";
+import btnHelp from "../../assets/botaoHelp.png";
 
 import { FaStopwatch } from "react-icons/fa";
 
@@ -11,6 +12,17 @@ import { Swiper, SwiperSlide } from "swiper/react";
 export function Academias() {
   const { id } = useParams();
   const [academia, setAcademia] = useState<AcademiaProps>();
+  const [academias, setAcademias] = useState<AcademiaProps[]>([]);
+  const [sliderPreView, setSliderPreView] = useState<number>(4);
+
+  useEffect(() => {
+    async function getAcademias() {
+      const resposta = await api.get("/academias");
+      setAcademias(resposta.data);
+    }
+
+    getAcademias();
+  }, []);
 
   useEffect(() => {
     async function getAcademia() {
@@ -20,6 +32,24 @@ export function Academias() {
 
     getAcademia();
   }, [id]);
+
+  useEffect(() => {
+    function handleResize() {
+      if (window.innerWidth < 720) {
+        setSliderPreView(1);
+      } else {
+        setSliderPreView(4);
+      }
+    }
+
+    handleResize();
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
 
   return (
     <Container>
@@ -88,10 +118,107 @@ export function Academias() {
       {academia && (
         <section className="px-6">
           <Link to={`/planos/${academia.id}`}>
-            <button className="p-3 bg-yellow-400 w-full rounded-full font-medium text-lg mt-6 cursor-pointer hover:bg-yellow-500 transition-all max-sm:mt-6">
+            <button className="p-3 mb-12 bg-red-600 w-full text-white rounded-full font-medium text-lg mt-6 cursor-pointer hover:bg-red-700 transition-all max-sm:mt-6">
               Quero treinar aqui!
             </button>
           </Link>
+        </section>
+      )}
+
+      <section className="mt-4 flex flex-col justify-center items-center">
+        <h1 className="text-4xl font-medium">
+          Conheça <span className="text-yellow-500 font-bold">a equipe</span>
+        </h1>
+        <p className="mt-4 w-2/3 text-gray-600 flex justify-center items-center">
+          É bom contar com essa galera para tirar dúvidas, treinar pra valer,
+          bater um papinho...
+        </p>
+        <p className="mb-4 w-2/3 text-gray-600 flex justify-center items-center">
+          Identifique o pessoal pela cor da camiseta!
+        </p>
+        <div className="grid grid-cols-3 mt-12 max-sm:grid-cols-1 mb-6">
+          <div className="flex flex-col justify-center items-center max-sm:mb-8">
+            <img
+              src="https://assets3.smartfit.com.br/assets/locations-page-v4/recepcionista.png"
+              alt="Camiseta Recepcionista"
+            />
+            <p className="text-xl mt-2">Recepcionista</p>
+            <p className="text-gray-600 mt-2 text-center w-2/3 text-sm">
+              Dúvidas sobre a operação da academia? Traga pro balcão!
+            </p>
+          </div>
+
+          <div className="flex flex-col justify-center items-center max-sm:mb-8">
+            <img
+              src="https://assets3.smartfit.com.br/assets/locations-page-v4/professores.png"
+              alt="Camiseta Professores"
+            />
+            <p className="text-xl mt-2">Professores</p>
+            <p className="text-gray-600 mt-2 text-center w-2/3 text-sm">
+              Ele sabe tudo sobre exercício, monta treino, dá aula coletiva e te
+              ajuda a conquistar seus objetivos.
+            </p>
+          </div>
+
+          <div className="flex flex-col justify-center items-center max-sm:mb-8">
+            <img
+              src="https://assets3.smartfit.com.br/assets/locations-page-v4/lider.png"
+              alt="Camiseta Líder da Unidade"
+            />
+            <p className="text-xl mt-2">Líder de Unidade</p>
+            <p className="text-gray-600 mt-2 text-center w-2/3 text-sm">
+              É a pessoa responsável pelo bom funcionamento da unidade está
+              sempre disponível para ajudar!
+            </p>
+          </div>
+        </div>
+      </section>
+
+      <div className="flex w-full bg-yellow-100 mt-6 py-4 px-2 justify-center items-center rounded-lg mb-10">
+        <img src={btnHelp} alt="Botão de ajuda" className="mr-6 max-sm:w-24" />
+        <div className="flex flex-col">
+          <p className="text-3xl mb-2 max-sm:text-lg">
+            Se ficar com dúvida, a gente te ajuda!︎
+          </p>
+          <p className="text-gray-700 text-sm">
+            Todos os nossos equipamentos possuem o botão GM Help, que solicita
+            ajuda de um professor quando acionado.
+          </p>
+        </div>
+      </div>
+
+      {academia && (
+        <section key={academia.id} className="w-full mb-12 mt-4 text-center">
+          <h1 className="text-4xl font-medium mb-4">
+            Encontramos essas outras academias{" "}
+            <span className="text-yellow-500 font-bold">perto de você!</span>
+          </h1>
+          <Swiper
+            slidesPerView={sliderPreView}
+            pagination={{ clickable: true }}
+            navigation
+          >
+            {academias.map((academia) => (
+              <SwiperSlide key={academia.id} className="pl-3 pr-3">
+                <div className="bg-white rounded-lg mt-4 h-64 relative">
+                  <Link to={`/academias/${academia.id}`}>
+                    <img
+                      className="w-full rounded-tr-lg rounded-tl-lg mb-2 max-h-40 object-cover "
+                      src={academia.cover}
+                    />
+                  </Link>
+
+                  <p className="font-medium px-4">{academia.name}</p>
+                  <p className="pb-2 text-sm text-gray-500 px-3 mt-1">
+                    {academia.address}
+                  </p>
+                  <p className="absolute bg-white text-gray-500 py-1 px-2 rounded-full top-1 left-1 text-sm">
+                    500m
+                  </p>
+                </div>
+              </SwiperSlide>
+            ))}
+          </Swiper>
         </section>
       )}
     </Container>
